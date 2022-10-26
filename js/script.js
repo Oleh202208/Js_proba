c7be432edf8320c4f2a988a381668ae7bbb502aa
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-10-26 12:21:19 +0200

    mod 1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use Strict";
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// pricePerItem = 3500;
+// productName = "Repair droid";
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+
+
+const productName = "Droid";
+const pricePerItem = 3500;
+const message = "You picked Droid, price per item is 3500 credits";
+
+
+function sayHi() {
+  console.log("Hello, this is my first function!")
+}
+sayHi();
