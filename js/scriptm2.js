645af2b320ce165cbc6e0b8bb4754cc697e58ec2
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-01 16:55:27 +0100

    Завдання 9 +

--- js/scriptm2.js
+++ js/scriptm2.js
@@ -15,3 +15,66 @@
 // for (const character of string) {
 //   console.log(character);
 // }
+
+//   3
+
+// function checkStorage(available, ordered) {
+  // Change code below this line
+
+  // if (ordered === 0) {
+  //   return "Your order is empty!";
+  // } if (ordered > available) {
+  //  return "Your order is too large, not enough goods in stock!";
+  // } 
+  //     return "The order is accepted, our manager will contact you";
+
+
+  // return
+  // Change code above this line
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+// 5
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits.length - 1;
+// console.log(fruits[lastElement]);
+
+// 7
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const fruitsArrayLength = fruits.length;
+// // console.log(fruits.length);
+// console.log(fruitsArrayLength);
+
+// 8
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+// console.log(lastElementIndex);
+// console.log(fruits[lastElementIndex ]);
+
+// 9
+
+function getExtremeElements(array) {
+  // Change code below this line
+
+return [array[0], 
+array[array.length-1]];
+}
+console.log(getExtremeElements([1, 2, 3, 4, 5]));
+console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
