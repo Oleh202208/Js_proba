d7ad100737378b0af1546dc06a5a1d28c55943ff
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-07 13:55:28 +0100

    4+

--- js/script3.js
+++ js/script3.js
@@ -25,3 +25,18 @@
 // console.log(apartment);
 
 // 3
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = 4;
+// const aptDescr = "Spacious apartment in the city center";
+// const aptPrice = 2153;
+// const aptTags = ["premium", "promoted", "top"];
+
+// 4
