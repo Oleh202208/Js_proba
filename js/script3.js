6ca90410f04aba6540afdd2667009600d2188c06
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-07 16:36:08 +0100

    5+

--- js/script3.js
+++ js/script3.js
@@ -39,4 +39,26 @@
 // const aptPrice = 2153;
 // const aptTags = ["premium", "promoted", "top"];
 
-// 4
+// // 4
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[2];
+
+// 
