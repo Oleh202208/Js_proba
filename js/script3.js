979d99053f828b450c1ada0416f05762b19fc0e5
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-09 15:31:17 +0100

    19+

--- js/script3.js
+++ js/script3.js
@@ -234,4 +234,46 @@
 // console.log(rgbColors)
 
 // 18
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+
+// for(const product of products) {
+//   if (product.name === productName) {
+//     return product.price;
+//   }
+// }
+// return null;
+// }
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"))
+// console.log(getProductPrice("Scanner"))
+// console.log(getProductPrice("Engine"))
+
+// 19
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+
+// let propertyValues = [];
+//   for (let product of products) {
+//     const keys = Object.keys(product);
+//     if (keys.includes(propName)) {
+//       propertyValues.push(product[propName]);
+//     }
+//   }
+// return propertyValues;
+// }
+
+// 20
 
