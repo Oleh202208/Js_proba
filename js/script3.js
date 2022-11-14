3c2f9c99fea6d923c84e80c817bb84bcd8c20f85
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-14 21:39:45 +0100

    41 +

--- js/script3.js
+++ js/script3.js
@@ -510,3 +510,43 @@
 // };
 
 
+// 41
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    for(const potion of this.potions)
+      if(potion.name === newPotion.name) {
+    
+      return `Error! Potion ${newPotion.name} is already in your inventory!`;
+    }
+
+    this.potions.push(newPotion);
+    return this.potions;
+  },
+  removePotion(potionName) {
+    
+    for(const potion of this.potions){
+      if(potionName === potion.name){
+        const potionIndex = this.potions.indexOf(potion);
+        this.potions.splice(potionIndex, 1);   
+      }
+    }
+      return `Potion ${potionName} is not in inventory!`;
+  },
+  updatePotionName(oldName, newName) {
+    for(const potion of this.potions)
+      if(oldName === potion.name){
+        potion.name = newName
+        return this.potions;
+      }
+  },
+};
