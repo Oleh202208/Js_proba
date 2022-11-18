ed902ed1882acb203a3a97c8d0367ca04a9e76aa
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-18 16:23:37 +0100

    48+

--- js/script4.js
+++ js/script4.js
@@ -335,3 +335,45 @@
 //   };
 
 // 45
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   {
+//     title: "The Dreams in the Witch House",
+//     author: "Howard Lovecraft",
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// const names = books.filter(books => books.rating > 8)
+//   .map(books => books.author)
+// .sort((a, b) => a.localeCompare(b));
+// console.log(names)
+
+// 47
+
+// const getSortedFriends = users => {
+//   return users.flatMap(users => users.friends)
+//   .filter((friend, index, array) => array.indexOf(friend) === index)
+//     .sort((a, b) => a.localeCompare(b))
+//   };
+  
+// 48
+const getTotalBalanceByGender = (users, gender) => {
+  return [...users].filter(users => users.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0)    
+  };
