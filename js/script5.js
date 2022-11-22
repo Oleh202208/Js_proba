f64faa3d6f658a561a403600227be42996a82367
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-22 19:14:23 +0100

    js 5

---
+++ js/script5.js
@@ -0,0 +1,9 @@
+// // Петя біжить швидко, тому що Петя намагається зловити поїзд.
+// const petya = {
+//   username: "Petya",
+//   showName() {
+//     console.log(petya.username);
+//   },
+// };
+
+// petya.showName();
