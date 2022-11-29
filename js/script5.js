39b82eb678f962a110c0e9d8c28dae574a4802ed
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-11-29 17:38:54 +0100

    лекція 5 до bind

--- js/script5.js
+++ js/script5.js
@@ -48,51 +48,134 @@
 // console.log(Car.checkPrice(bmw.price));
 
 // 20===
-class User {
-  email;
+// class User {
+//   email;
 
-  constructor(email) {
-    this.email = email;
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//   }
+// blacklistedEmails = [];
+//  blacklist(email){
+//    this.blacklistedEmails.push(email);
+//  }
+//   isBlacklisted(email){
+//     return this.blacklistedEmails.includes(email);
+//   }
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+
+
+// const obj = {
+//   name: `user Name`,
+//   info: {
+//     age: 32,
+//     nickName: `Petro`,
+//     getnickName () {
+//       console.log(this.nickName)
+//     }
+//   },
+//   getName() {
+//     console.log(this.name, this.age);
+//   }
+// }
+// obj.info.getnickName()
+
+// obj.getName()
+
+// const user = {
+//   name: `Test user`,
+//   age: 18,
+//   metod: obj.getName
+
+// }
+// user.metod()
+
+// const user2 = {
+//   name: `Okun`,
+//   age: `Молюск`,
+//   fish: obj.getName
+// }
+// user2.fish()
+
+// const arrow = () => {
+//   console.log(this)
+// };
+// const obj = {
+//   name: `userName`,
+//   MyThis: this,
+//   getname: () => {
+// console.log(`in arrow function`, this.name);
+//   }
+
+// }
+// obj.getname();
+// console.log(`name`, name);
+// console.log(obj.MyThis);
+
+// 'use strict'
+
+// function foo () {
+// console.log(`declaration`, this);
+// }
+// foo()
+// const expression = function () {
+//   console.log(`expression`, this);
+// }
+// expression()
+
+// const arrow = () => {
+//   console.log(`arrow`, this)
+// }
+// arrow()
+
+
+
+const objA = {
+  name: `User A`,
+    getName () {
+      console.log(this.name)
+    }
   }
 
-  get email() {
-    return this.email;
+const objB = {
+  name: `User B`,
   }
 
-  set email(newEmail) {
-    this.email = newEmail;
+
+const objC = {
+  name: `User C`,
   }
-}
-class Admin extends User {
-  // Change code below this line
-
-  static AccessLevel = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
-
-  constructor({ email, accessLevel }) {
-    super(email);
-    this.accessLevel = accessLevel;
-  }
-blacklistedEmails = [];
- blacklist(email){
-   this.blacklistedEmails.push(email);
- }
-  isBlacklisted(email){
-    return this.blacklistedEmails.includes(email);
-  }
-}
-
-const mango = new Admin({
-  email: "mango@mail.com",
-  accessLevel: Admin.AccessLevel.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.accessLevel); // "superuser"
-
-mango.blacklist("poly@mail.com");
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted("mango@mail.com")); // false
-console.log(mango.isBlacklisted("poly@mail.com")); // true
