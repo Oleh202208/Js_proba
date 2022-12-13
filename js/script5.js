26226838c55fecf60a5462cb99b6a2be88c3a3c2
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-13 16:31:52 +0100

    X 0 finish

--- js/script5.js
+++ js/script5.js
@@ -327,43 +327,69 @@
 
 
 const content = document.querySelector('.content');
-const wins = [
-  [1,2,3],
+
+const wins = [ 
+[1,2,3],
 [4,5,6],
 [7,8,9],
 [1,5,9],
 [3,5,7],
 [3,6,9],
 [1,4,7],
+[2,5,8],
 ];
 const stepX = [];
-const stepO = [];
-let markup = '';
+const step0 = [];
+
 let player = 'X';
-for(let i =1; i<= 9; i += 1) {
-  markup += `<div class='item' data-id='${i}'></div>`
+function createMarkup(){
+  let markup = '';
+
+  for(let i =1; i<= 9; i += 1) {
+    markup += `<div class='item' data-id='${i}'></div>`
+  }
+
+  content.innerHTML = markup;
 }
-content.insertAdjacentHTML('beforeend', markup);
+createMarkup()
+
 content.addEventListener('click', onClick);
 
 function onClick(evt) {
   if(!evt.target.classList.contains('item')) {
     return;
   }
+  console.log('this', evt.target.textContent);
   if(evt.target.textContent) {
     return;
   }
+
 const currentId = Number( evt.target.dataset.id);
+let result = false;
 
 if(player === 'X') {
   stepX.push(currentId)
+  result = isWinner(stepX);
 }else{
-  stepO.push(currentId)
+  step0.push(currentId)
+  result = isWinner(step0);
+}
+evt.target.textContent = player;
+
+if(result) {
+  chempion(player);
+  return;
+}
+  
+  player = player === 'X' ? '0' : 'X'; 
+
 }
 
-  evt.target.textContent = player;
-  player = player === 'X' ? 'O' : 'X'; 
-
+function chempion(player) {
+  setTimeout(()=> {
+    alert(player);
+    createMarkup();
+  }, 500)
 }
 
 function isWinner(arr) {
