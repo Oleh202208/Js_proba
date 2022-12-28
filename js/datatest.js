c1ec3b8164a29a375fcabb6bde2eb8d35c4f9b07
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-28 18:29:49 +0100

    data

---
+++ js/datatest.js
@@ -0,0 +1,32 @@
+const spam = document.querySelector('.js-spam')
+const clock = document.querySelector('.js-clock')
+const timeEnd = 7000;
+clock.textContent = `Реклама зникне через 7 c`;
+let count = 7;
+
+const id2 = setInterval(()=>{
+  count -=1
+  clock.textContent = `Реклама зникне через ${count} c`;
+}, 1000)
+
+const id = setTimeout(() => {
+spam.textContent = '';
+clock.textContent = '';
+clearInterval(id2)
+}, timeEnd)
+
+
+// const id = setTimeout(() => {
+//   for(let i =0; i<3; i +=1) {
+//     console.log('for in setTimeout', i)
+//   }
+// }, 1000)
+
+
+// const id2 = setInterval(()=>{
+//   for(let i =0; i<3; i +=1) {
+//     console.log('for in setIntervalt', i)
+//   }
+// }, 3000)
+// console.log(id);
+// console.log(id2);
--- js/script5.js
+++ js/script5.js
@@ -331,100 +331,221 @@
 
 
 
-const content = document.querySelector('.content');
+// const content = document.querySelector('.content');
 
-const wins = [ 
-[1,2,3],
-[4,5,6],
-[7,8,9],
-[1,5,9],
-[3,5,7],
-[3,6,9],
-[1,4,7],
-[2,5,8],
-];
+// const wins = [ 
+// [1,2,3],
+// [4,5,6],
+// [7,8,9],
+// [1,5,9],
+// [3,5,7],
+// [3,6,9],
+// [1,4,7],
+// [2,5,8],
+// ];
 
-const KEY_X = 'keyX';
-const KEY_0 = 'key0';
-const KEY_PLAYER = 'player';
-const KEY_COUNTER = 'counter';
-const endGame = 9;
-let counter = JSON.parse(localStorage.getItem(KEY_COUNTER)) || 0;
-let player = localStorage.getItem(KEY_PLAYER) || 'X';
+// const KEY_X = 'keyX';
+// const KEY_0 = 'key0';
+// const KEY_PLAYER = 'player';
+// const KEY_COUNTER = 'counter';
+// const endGame = 9;
+// let counter = JSON.parse(localStorage.getItem(KEY_COUNTER)) || 0;
+// let player = localStorage.getItem(KEY_PLAYER) || 'X';
 
-const stepX = JSON.parse(localStorage.getItem(KEY_X)) || [];
-const step0 = JSON.parse(localStorage.getItem(KEY_0)) || [];
+// const stepX = JSON.parse(localStorage.getItem(KEY_X)) || [];
+// const step0 = JSON.parse(localStorage.getItem(KEY_0)) || [];
 
-function autoComplite(){
-[...content.children].forEach(item =>{
-  const id = Number(item.dataset.id)
-  if(stepX.includes(id)){
-    item.textContent = 'X';
-  }else if(step0.includes(id)){
-    item.textContent = '0';
-  }
-})
-console.dir(content);
-}
+// function autoComplite(){
+// [...content.children].forEach(item =>{
+//   const id = Number(item.dataset.id)
+//   if(stepX.includes(id)){
+//     item.textContent = 'X';
+//   }else if(step0.includes(id)){
+//     item.textContent = '0';
+//   }
+// })
+// console.dir(content);
+// }
 
 
-function createMarkup(){
-  let markup = '';
-  for(let i =1; i<= 9; i += 1) {
-    markup += `<div class='item' data-id='${i}'></div>`
-  }
+// function createMarkup(){
+//   let markup = '';
+//   for(let i =1; i<= 9; i += 1) {
+//     markup += `<div class='item' data-id='${i}'></div>`
+//   }
 
-  content.innerHTML = markup;
-}
-createMarkup()
-autoComplite()
+//   content.innerHTML = markup;
+// }
+// createMarkup()
+// autoComplite()
 
-content.addEventListener('click', onClick);
+// content.addEventListener('click', onClick);
 
-function onClick(evt) {
-  if(!evt.target.classList.contains('item')) {
-    return;
-  }
-  console.log('this', evt.target.textContent);
-  if(evt.target.textContent) {
-    return;
-  }
+// function onClick(evt) {
+//   if(!evt.target.classList.contains('item')) {
+//     return;
+//   }
+//   console.log('this', evt.target.textContent);
+//   if(evt.target.textContent) {
+//     return;
+//   }
 
-const currentId = Number( evt.target.dataset.id);
-let result = false;
-counter+=1;
-if(player === 'X') {
-  stepX.push(currentId);
-  localStorage.setItem(KEY_X, JSON.stringify(stepX))
-  result = isWinner(stepX);
-}else{
-  step0.push(currentId)
-  localStorage.setItem(KEY_0, JSON.stringify(step0))
-  result = isWinner(step0);
-}
-evt.target.textContent = player;
+// const currentId = Number( evt.target.dataset.id);
+// let result = false;
+// counter+=1;
+// if(player === 'X') {
+//   stepX.push(currentId);
+//   localStorage.setItem(KEY_X, JSON.stringify(stepX))
+//   result = isWinner(stepX);
+// }else{
+//   step0.push(currentId)
+//   localStorage.setItem(KEY_0, JSON.stringify(step0))
+//   result = isWinner(step0);
+// }
+// evt.target.textContent = player;
 
-if(result) {
-  chempion(player);
-  return;
-}
-  if(content === endGame) {
-    chempion('RESTART GAME')
-    return;
-  }
-  player = player === 'X' ? '0' : 'X'; 
-localStorage.setItem(KEY_PLAYER, player)
-localStorage.setItem(KEY_COUNTER, JSON.stringify(counter))
-}
+// if(result) {
+//   chempion(player);
+//   return;
+// }
+//   if(content === endGame) {
+//     chempion('RESTART GAME')
+//     return;
+//   }
+//   player = player === 'X' ? '0' : 'X'; 
+// localStorage.setItem(KEY_PLAYER, player)
+// localStorage.setItem(KEY_COUNTER, JSON.stringify(counter))
+// }
 
-function chempion(player) {
-  setTimeout(()=> {
-    alert(player);
-    createMarkup();
-    localStorage.clear();
-  }, 500)
-}
+// function chempion(player) {
+//   setTimeout(()=> {
+//     alert(player);
+//     createMarkup();
+//     localStorage.clear();
+//   }, 500)
+// }
 
-function isWinner(arr) {
-return wins.some(item => item.every(id =>arr.includes(id)))
-}
+// function isWinner(arr) {
+// return wins.some(item => item.every(id =>arr.includes(id)))
+// }
+
+
+// =================    Варіант Артема ==========================
+
+
+
+// const content = document.querySelector('.content');
+// const KEY_X = 'keyX';
+// const KEY_O = 'keyO';
+// const KEY_PLAYER = "player";
+// const KEY_COUNTER = "counter"
+// const endGame = 9;
+// let counter = JSON.parse(localStorage.getItem(KEY_COUNTER)) || 0;
+// let player = localStorage.getItem(KEY_PLAYER) || 'X';
+
+// const stepX = JSON.parse(localStorage.getItem(KEY_X)) || [];
+// const stepO = JSON.parse(localStorage.getItem(KEY_O)) || [];
+
+
+
+// function autoComplite() {
+//     [...content.children].forEach(item => {
+//         const id = Number(item.dataset.id)
+//         if (stepX.includes(id)) {
+//             item.textContent = "X";
+//         } else if (stepO.includes(id)) {
+//             item.textContent = "O";
+//         }
+//     })
+//     console.dir(content);
+// }
+
+// function createMarkup() {
+//     let markup = '';
+
+//     for (let i = 1; i <= 9; i += 1) {
+//         markup += `<div class="item" data-id="${i}"></div>`;
+//     }
+
+//     content.innerHTML = markup;
+// }
+// createMarkup();
+// autoComplite()
+
+// content.addEventListener('click', onClick);
+
+// function onClick(evt) {
+//     if (!evt.target.classList.contains('item')) {
+//         return;
+//     }
+
+//     if (evt.target.textContent) {
+//         return;
+//     }
+
+//     const currentId = Number(evt.target.dataset.id);
+//     let result = false;
+//     counter += 1;
+//     if (player === "X") {
+//         stepX.push(currentId);
+//         localStorage.setItem(KEY_X, JSON.stringify(stepX))
+//         result = isWinner(stepX)
+
+//     } else {
+//         stepO.push(currentId)
+//         localStorage.setItem(KEY_O, JSON.stringify(stepO))
+//         result = isWinner(stepO)
+//     }
+
+//     evt.target.textContent = player;
+
+//     if (result) {
+//         chempion(player);
+//         return;
+//     }
+
+//     if (counter === endGame) {
+//         chempion("RESTART GAME")
+//         return;
+//     }
+
+//     player = player === "X" ? "O" : "X";
+//     localStorage.setItem(KEY_PLAYER, player)
+//     localStorage.setItem(KEY_COUNTER, JSON.stringify(counter))
+// }
+
+// const wins = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
+//     [1, 5, 9],
+//     [3, 5, 7]
+// ];
+
+// function isWinner(arr) {
+//     console.log("історія ходів", arr);
+//     return wins.some(item => {
+//         console.log("item", item);
+//         const result = item.every(id => {
+//             console.log("id", id);
+//             return arr.includes(id)
+//         })
+//         console.log('result', result);
+//         return result
+//     })
+// }
+
+// function isWinner(arr) {
+//     return wins.some((item) => item.every(id => arr.includes(id)))
+// }
+
+// function chempion(player) {
+//     setTimeout(() => {
+//         alert(player);
+//         createMarkup();
+//         localStorage.clear()
+//     }, 500)
+// }
