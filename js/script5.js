181b54a85f9c7a9ff97feb2432f9491d0e435684
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-28 12:51:47 +0100

    kubik

--- js/script5.js
+++ js/script5.js
@@ -331,72 +331,100 @@
 
 
 
-// const content = document.querySelector('.content');
+const content = document.querySelector('.content');
 
-// const wins = [ 
-// [1,2,3],
-// [4,5,6],
-// [7,8,9],
-// [1,5,9],
-// [3,5,7],
-// [3,6,9],
-// [1,4,7],
-// [2,5,8],
-// ];
-// const stepX = [];
-// const step0 = [];
+const wins = [ 
+[1,2,3],
+[4,5,6],
+[7,8,9],
+[1,5,9],
+[3,5,7],
+[3,6,9],
+[1,4,7],
+[2,5,8],
+];
 
-// let player = 'X';
-// function createMarkup(){
-//   let markup = '';
+const KEY_X = 'keyX';
+const KEY_0 = 'key0';
+const KEY_PLAYER = 'player';
+const KEY_COUNTER = 'counter';
+const endGame = 9;
+let counter = JSON.parse(localStorage.getItem(KEY_COUNTER)) || 0;
+let player = localStorage.getItem(KEY_PLAYER) || 'X';
 
-//   for(let i =1; i<= 9; i += 1) {
-//     markup += `<div class='item' data-id='${i}'></div>`
-//   }
+const stepX = JSON.parse(localStorage.getItem(KEY_X)) || [];
+const step0 = JSON.parse(localStorage.getItem(KEY_0)) || [];
 
-//   content.innerHTML = markup;
-// }
-// createMarkup()
+function autoComplite(){
+[...content.children].forEach(item =>{
+  const id = Number(item.dataset.id)
+  if(stepX.includes(id)){
+    item.textContent = 'X';
+  }else if(step0.includes(id)){
+    item.textContent = '0';
+  }
+})
+console.dir(content);
+}
 
-// content.addEventListener('click', onClick);
 
-// function onClick(evt) {
-//   if(!evt.target.classList.contains('item')) {
-//     return;
-//   }
-//   console.log('this', evt.target.textContent);
-//   if(evt.target.textContent) {
-//     return;
-//   }
+function createMarkup(){
+  let markup = '';
+  for(let i =1; i<= 9; i += 1) {
+    markup += `<div class='item' data-id='${i}'></div>`
+  }
 
-// const currentId = Number( evt.target.dataset.id);
-// let result = false;
+  content.innerHTML = markup;
+}
+createMarkup()
+autoComplite()
 
-// if(player === 'X') {
-//   stepX.push(currentId)
-//   result = isWinner(stepX);
-// }else{
-//   step0.push(currentId)
-//   result = isWinner(step0);
-// }
-// evt.target.textContent = player;
+content.addEventListener('click', onClick);
 
-// if(result) {
-//   chempion(player);
-//   return;
-// }
-  
-//   player = player === 'X' ? '0' : 'X'; 
+function onClick(evt) {
+  if(!evt.target.classList.contains('item')) {
+    return;
+  }
+  console.log('this', evt.target.textContent);
+  if(evt.target.textContent) {
+    return;
+  }
 
-// }
+const currentId = Number( evt.target.dataset.id);
+let result = false;
+counter+=1;
+if(player === 'X') {
+  stepX.push(currentId);
+  localStorage.setItem(KEY_X, JSON.stringify(stepX))
+  result = isWinner(stepX);
+}else{
+  step0.push(currentId)
+  localStorage.setItem(KEY_0, JSON.stringify(step0))
+  result = isWinner(step0);
+}
+evt.target.textContent = player;
 
-// function chempion(player) {
-//   setTimeout(()=> {
-//     alert(player);
-//     createMarkup();
-//   }, 500)
-// }
+if(result) {
+  chempion(player);
+  return;
+}
+  if(content === endGame) {
+    chempion('RESTART GAME')
+    return;
+  }
+  player = player === 'X' ? '0' : 'X'; 
+localStorage.setItem(KEY_PLAYER, player)
+localStorage.setItem(KEY_COUNTER, JSON.stringify(counter))
+}
 
-// function isWinner(arr) {
-// return wins.some(item => item.every(id =>arr.includes(id)))
-// }
+function chempion(player) {
+  setTimeout(()=> {
+    alert(player);
+    createMarkup();
+    localStorage.clear();
+  }, 500)
+}
+
+function isWinner(arr) {
+return wins.some(item => item.every(id =>arr.includes(id)))
+}
